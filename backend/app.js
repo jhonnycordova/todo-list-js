071dfdfeb0dc4220eea290b78f0318590062e1da
Jhonny Cordova <jhonny.cordova@dsarhoya.cl>
2021-07-09 09:36:15 -0400

    Using config file.

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const { config } = require('./config/config');
+// app
+const app = express();
+
+// redirect
+app.get("/", function(req, res) {
+  res.send("Hola Mundo");
+});
+
+// server
+const server = app.listen(config.port || 3000, function() {
+  console.log(`Listening http://localhost:${server.address().port}`);
+});

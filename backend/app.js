dff4063f633084433b579cc650fed32d04f0761f
Jhonny Cordova <jhonny.cordova@dsarhoya.cl>
2021-07-09 17:01:09 -0400

    rollbar.js that export an instance of Rollbar class by default.

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,19 @@
 const express = require('express');
 const { config } = require('./config/config');
-const Rollbar = require('rollbar');
-const rollbar = new Rollbar({
-  accessToken: config.rollbarToken,
-  captureUncaught: true,
-  captureUnhandledRejections: true,
-  environment: config.environment,
-});
+const Rollbar = require('./lib/rollbar');
 // app
 const app = express();
 
-app.get('/', function (req, res) {
-  // rollbar.info('Awesome, this is a info log!');
+app.get('/', (req, res) => {
+  // Rollbar.log('Awesome, this is a info log!');
   res.send('Hola Mundo');
 });
 
 // this will send exception to rollbar account
-app.use(rollbar.errorHandler());
+app.use(Rollbar.errorHandler());
 
 // server
-const server = app.listen(config.port || 3000, function () {
+const server = app.listen(config.port || 3000, () => {
   // eslint-disable-next-line no-console
   console.log(`Listening http://localhost:${server.address().port}`);
 });

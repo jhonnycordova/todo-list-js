eb41a7ce865510c135ba08e012fcaa08fea06524
Jhonny Cordova <jhonny.cordova@dsarhoya.cl>
2021-07-09 10:41:21 -0400

    Using Rollbar at Backend project

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const { config } = require('./config/config');
+const Rollbar = require('rollbar');
+const rollbar = new Rollbar({
+  accessToken: config.rollbarToken,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+  environment: config.environment,
+});
 // app
 const app = express();
 
 app.get('/', function (req, res) {
+  // rollbar.info('Awesome, this is a info log!');
   res.send('Hola Mundo');
 });
+
+// this will send exception to rollbar account
+app.use(rollbar.errorHandler());
 
 // server
 const server = app.listen(config.port || 3000, function () {
--- backend/config/config.js
+++ backend/config/config.js
@@ -1,8 +1,10 @@
-require("dotenv").config();
+require('dotenv').config();
 
 const config = {
-  dev: process.env.NODE_ENV !== "production",
-  port: process.env.PORT
+  dev: process.env.NODE_ENV !== 'production',
+  port: process.env.PORT,
+  rollbarToken: process.env.ROLLBAR_TOKEN,
+  environment: process.env.NODE_ENV,
 };
 
 module.exports = { config };

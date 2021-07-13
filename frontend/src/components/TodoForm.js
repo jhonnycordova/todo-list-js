997afe3b6fe0041e9438b6f06838721d5eb09ab5
Ángel D Barrios <angeldbarriosg11@gmail.com>
2021-07-13 10:53:16 -0500

    form validation

--- frontend/src/components/TodoForm.js
+++ frontend/src/components/TodoForm.js
@@ -8,6 +8,7 @@ export default (props) => {
 
   const handleClick = () => {
     const inputValue = inputRef.current.value;
+    if (inputValue === '') return;
     props.addNewTodo(inputValue);
   };
 

333a6528b2b95939cf32d1e6f3e30c8b56797b06
Ángel D Barrios <angeldbarriosg11@gmail.com>
2021-07-14 14:52:32 -0500

    add feature for error message

--- frontend/src/components/TodoForm.js
+++ frontend/src/components/TodoForm.js
@@ -1,20 +1,27 @@
 import React, { useState } from 'react';
 import {
-  FormControl, Input, InputLabel, Button,
+  FormControl, Input, InputLabel, Button, FormHelperText,
 } from '@material-ui/core';
 
 export default ({ onAddTodo }) => {
   const [isButtonDisable, setIsButtonDisable] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(undefined);
   const inputRef = React.createRef();
 
-  const handleClick = () => {
+  const handleClick = (event) => {
     const inputValue = inputRef.current.value;
     if (inputValue === '') return;
     setIsButtonDisable(false);
+    event.preventDefault();
 
-    onAddTodo(inputValue, () => {
+    onAddTodo(inputValue, (error) => {
       setIsButtonDisable(true);
-      inputRef.current.value = '';
+      if (error) {
+        setErrorMessage(error.message);
+      } else {
+        inputRef.current.value = '';
+        setErrorMessage(undefined);
+      }
     });
   };
 
@@ -22,6 +29,7 @@ export default ({ onAddTodo }) => {
     <FormControl>
       <InputLabel htmlFor="new-todo-input">Todo Name</InputLabel>
       <Input id="new-todo-input" ref={inputRef} />
+      {errorMessage && errorMessage !== '' && <FormHelperText error>{errorMessage}</FormHelperText>}
       <Button color="primary" onClick={handleClick} disabled={isButtonDisable}>Add</Button>
     </FormControl>
   );

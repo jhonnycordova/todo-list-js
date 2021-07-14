750e29b10e1a7431d30144235c7e84d29b29ae78
Jhonny Cordova <jhonny.cordova@dsarhoya.cl>
2021-07-14 17:59:23 -0400

    TodoList Test

--- frontend/src/components/TodoList.test.js
+++ frontend/src/components/TodoList.test.js
@@ -7,3 +7,13 @@ test('renders todo title', () => {
   const title = screen.getByText(/To Do List/i);
   expect(title).toBeInTheDocument();
 });
+
+test('renders toDos children', () => {
+  const toDoList = [
+    { id: 1, task: 'task1 ', isCompleted: false },
+    { id: 2, task: 'task2 ', isCompleted: true },
+  ];
+  const { queryByDisplayValue } = render(<TodoList toDoList={toDoList} />);
+  expect(queryByDisplayValue(/task1/i)).toBeInTheDocument();
+  expect(queryByDisplayValue(/task2/i)).toBeInTheDocument();
+});

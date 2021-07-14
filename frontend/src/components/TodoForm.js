2f18a4d2828ebf4bed51bffb69b015e75d55b774
Ángel D Barrios <angeldbarriosg11@gmail.com>
2021-07-14 14:03:36 -0500

    adding feature for button disabling

--- frontend/src/components/TodoForm.js
+++ frontend/src/components/TodoForm.js
@@ -1,22 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   FormControl, Input, InputLabel, Button,
 } from '@material-ui/core';
 
-export default (props) => {
+export default ({ onAddTodo }) => {
+  const [isButtonDisable, setIsButtonDisable] = useState(false);
   const inputRef = React.createRef();
 
   const handleClick = () => {
     const inputValue = inputRef.current.value;
     if (inputValue === '') return;
-    props.addNewTodo(inputValue);
+    setIsButtonDisable(false);
+
+    onAddTodo(inputValue, () => {
+      setIsButtonDisable(true);
+      inputRef.current.value = '';
+    });
   };
 
   return (
     <FormControl>
       <InputLabel htmlFor="new-todo-input">Todo Name</InputLabel>
       <Input id="new-todo-input" ref={inputRef} />
-      <Button color="primary" onClick={handleClick}>Add</Button>
+      <Button color="primary" onClick={handleClick} disabled={isButtonDisable}>Add</Button>
     </FormControl>
   );
 };

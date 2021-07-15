612ed8f2b03baf14dcb6c332e6b0ea877f33805f
Jhonny Cordova <jhonny.cordova@dsarhoya.cl>
2021-07-15 09:35:56 -0400

    one test

---
+++ frontend/src/components/Todo.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Todo from '../components/Todo';
+import userEvent from '@testing-library/user-event';
+
+test('call right function when user click on delete', () => {
+  const toDo = { id: 1, task: 'task1 ', isCompleted: false };
+  const onDeleteToDoMock = jest.fn();
+  const { getByRole } = render(<Todo toDo={toDo} onDeleteToDo={onDeleteToDoMock} />);
+  const deleteButton = getByRole('button');
+  userEvent.click(deleteButton);
+  expect(onDeleteToDoMock).toBeCalled();
+});
+
+test('call right function when user click on checkbox to toggle todo status', () => {
+  const toDo = { id: 1, task: 'task1 ', isCompleted: false };
+  const onHandleToggleMock = jest.fn();
+  const { getByRole } = render(<Todo toDo={toDo} onHandleToggle={onHandleToggleMock} />);
+  const checkbox = getByRole('checkbox');
+  userEvent.click(checkbox);
+  expect(onHandleToggleMock).toBeCalled();
+});
+
+// appears line-through when user click checkbox on uncompleted task

b808f2a80bd740fa05de8415a969fb6c8c27492b
Julieth Gomez <julieth15gomez@gmail.com>
2021-07-15 09:05:54 -0500

    Delete unused code

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,6 @@ const todoRoutes = require('./controllers/todo');
 
 const app = express();
 app.use(express.json());
-app.use('', todoRoutes);
-app.get('/', (req, res) => {
-  // Rollbar.log('Awesome, this is a info log!');
-  res.send('Hola Mundo');
-});
-
+app.use('/', todoRoutes);
 app.use(Rollbar.errorHandler());
 module.exports = app;
